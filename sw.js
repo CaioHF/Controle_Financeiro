108394eba9f8db5322953cb2f311388926047b49
Caio-Ferencine <caiohf01@gmail.com>
2026-01-08 10:24:48 -0300

    relatorios de ganhos e gastos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'financas-v13'; // Mudei para v13 para forçar atualização
+const CACHE_NAME = 'financas-v14'; // Mudei para v14 para forçar atualização
 const urlsToCache = [
   './',
   './index.html',
